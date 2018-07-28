e77f510ca2885f9988d828dd362f189017a1b72b
Suparn B <rockycool00007@gmail.com>
2018-07-28 15:40:12 -0700

    Typed js

---
+++ app/scripts/app.js
@@ -0,0 +1,54 @@
+$(function(){
+  $("#starter-text").typed({
+    strings: ["THAT'S ME!! ^1055 VIEW MY PROFILE BELOW"], // ^{number} denotes time in ms to wait
+
+    // typing speed
+    typeSpeed: 70,
+    
+    // time before typing starts
+    startDelay: 0,
+    
+    // backspacing speed
+    backSpeed: 0,
+    
+    // time before backspacing
+    backDelay: 50,
+    
+    // loop
+    loop: false,
+    
+    // false = infinite
+    loopCount: false,
+    
+    // show cursor
+    showCursor: true,
+    
+    // character for cursor
+    cursorChar: "",
+    
+    // attribute to type (null == text)
+    attr: null,
+    
+    // either html or text
+    contentType: 'html',
+    
+    // call when done callback function
+    callback: function() {
+
+       $('.resource').delay(500).fadeIn('slow');
+      setTimeout (function () {
+        $('#starter-text + .typed-cursor').css({'opacity': '0', 'animation': 'none'});
+      }, 1300);
+
+    },
+    
+    // starting callback function before each string
+    preStringTyped: function() {},
+    
+    //callback for every typed string
+    onStringTyped: function() {},
+    
+    // callback for reset
+    resetCallback: function() {}
+  });
+});
